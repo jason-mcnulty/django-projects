2a715d80f3515abe80b448c7584ba56642747aea
jason-mcnulty <jason.mcnulty@gmail.com>
2019-03-11 14:07:15 -0400

    adding files

---
+++ django-class-projects/the_weather/weather/views.py
@@ -0,0 +1,27 @@
+import requests
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+
+    #url = 'http://www.omdbapi.com/?t=back+to+the+future&apikey=676ef45a'
+
+    url = 'http://www.omdbapi.com/?t={}&apikey=676ef45a'
+    movie = 'xxx'
+
+    r = requests.get(url.format(movie)).json()
+    #print(r.text)
+
+
+    #movie_info = {'insert_me': "Hello I am looking at this"}
+    movie_info = {
+
+    'Title' : r['Title'],
+    'Year' :  r['Year'],
+    'Actors' : r['Actors'],
+    'Poster' : r['Poster'],
+
+    }
+
+    #context = {'city_weather' : movie_info}
+    return render(request, 'weather/index.html', context=movie_info)
