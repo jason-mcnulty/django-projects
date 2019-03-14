7b61130eeda31c7283029f606141a01b118fe22f
jason-mcnulty <jason.mcnulty@gmail.com>
2019-03-14 12:32:26 -0400

    adding files

--- access_security/index/views.py
+++ access_security/index/views.py
@@ -6,7 +6,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index/index.html')
 
-
 def citrix(request):
     return render(request, 'citrix/index.html')
 
--- access_security/pulse_secure/urls.py
+++ access_security/pulse_secure/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from . import views
+app_name = 'pulse_secure'
 
 urlpatterns = [
-    path('', views.pulse_secure),
+    path('', views.pulse_secure, name='pulse_secure'),
 ]
