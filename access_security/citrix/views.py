e3450f350d814cb23287b96051d73d7092403031
jason_mcnulty <Jason.mcnulty@gmail.com>
2019-03-15 18:58:06 -0400

    adding files

--- access_security/citrix/views.py
+++ access_security/citrix/views.py
@@ -1,3 +1,7 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
@@ -5,5 +9,46 @@ from django.shortcuts import render, HttpResponse
 #     return HttpResponse("This is the Citrix page citrix.views.py.")
 
 
+
+
+
+
+
 def citrix(request):
+
+    ssl = 'https://'
+    host1 = 'va10pctxns001.mgmt.wellpoint.com'
+    api = '/nitro/v1/stat/'
+    service = 'aaa'
+
+    ###########################################################################################
+    # va10pctxns001.mgmt.wellpoint.com
+    ###########################################################################################
+    #assigning the variable url to the URL of the API
+    url =  ssl + host1 + api + service
+
+    #assigning the username and password to a variable by calling upon the function
+    username = '@@@@@'
+    passwd = '@@@@@'
+
+    #this is where the magic happens...
+    myResponse = requests.get(url,auth=HTTPBasicAuth((username), (passwd)), verify=False)
+    jData = json.loads(myResponse.content)
+
+    for aaa_id,aaa_info in jData.items():
+        print('!')
+
+
+
+
+    for key in aaa_info:
+        print('@')
+
+    print (aaa_info['aaacuricaonlyconn'])
+    print (aaa_info['aaacuricasessions'])
+
+
+
+
+
     return render(request, 'citrix/index.html')
