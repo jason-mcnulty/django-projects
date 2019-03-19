bf428cdf2a4334a213270372dd81c9e423a15818
Jason P. McNulty <AC89458@LC02R835TFVH8>
2019-03-19 17:22:48 -0400

    adding files

--- access_security/pulse_secure/views.py
+++ access_security/pulse_secure/views.py
@@ -11,51 +11,93 @@ def pulse_secure(request):
 
 
     ###########################################################################################
-    # va10pctxns001.mgmt.wellpoint.com
+    # vappsavpn01.mgmt.wellpoint.com
     ###########################################################################################
     ssl = 'https://'
     host = 'vappsavpn01-int.internal.das'
     check = '/dana-na/healthcheck/healthcheck.cgi?status=all'
 
-    # Here, we're just importing both Beautiful Soup and the Requests library
     url = ssl + host + check
-    # this is the url that we've already determined is safe and legal to scrape from.
+    page_response = requests.get(url, verify=False)
+    page_content = BeautifulSoup(page_response.text, "html.parser")
 
-    username = 'ac89458'
-    passwd = 'Jayantliz2'
-    #functon to pull username from txt file on local machine
-    # def get_username():
-    #     f = open('/Users/AC89458/Documents/ad_username.txt', 'r')
-    #     my_username = f.read().strip()
-    #     return my_username
-    #     f.close()
+    ###########################################################################################
+    # vappsavpn02.mgmt.wellpoint.com
+    ###########################################################################################
+    ssl = 'https://'
+    host = 'vappsavpn02-int.internal.das'
+    check = '/dana-na/healthcheck/healthcheck.cgi?status=all'
 
-    #functon to pull password from txt file on local machine
-    # def get_passwd():
-    #     f = open('/Users/AC89458/Documents/ad_pausewd.txt', 'r')
-    #     my_passwd = f.read().strip()
-    #     return my_passwd
-    #     f.close()
+    url = ssl + host + check
+    page_response = requests.get(url, verify=False)
+    page_content = BeautifulSoup(page_response.text, "html.parser")
 
-    #assigning the username and password to a variable by calling upon the function
-    # username = get_username()
-    # passwd = get_passwd()
 
-    page_response = requests.get(url,auth=HTTPBasicAuth((username), (passwd)), verify=False)
 
-    # page_response = requests.get(url, timeout=5)
-    # here, we fetch the content from the url, using the requests library
-    page_content = BeautifulSoup(page_response.content, "html.parser")
-    #we use the html parser to parse the url content and store it in a variable.
+    ###########################################################################################
+    # vappsavpn03.mgmt.wellpoint.com
+    ###########################################################################################
+    ssl = 'https://'
+    host = 'vappsavpn03-int.internal.das'
+    check = '/dana-na/healthcheck/healthcheck.cgi?status=all'
 
-    print (page_content)
+    url = ssl + host + check
+    page_response = requests.get(url, verify=False)
+    page_content = BeautifulSoup(page_response.text, "html.parser")
+
+
+    ###########################################################################################
+    # moppsavpn01.mgmt.wellpoint.com
+    ###########################################################################################
+    ssl = 'https://'
+    host = 'moppsavpn01-int.internal.das'
+    check = '/dana-na/healthcheck/healthcheck.cgi?status=all'
+
+    url = ssl + host + check
+    page_response = requests.get(url, verify=False)
+    page_content = BeautifulSoup(page_response.text, "html.parser")
+
+
+    ###########################################################################################
+    # moppsavpn02.mgmt.wellpoint.com
+    ###########################################################################################
+    ssl = 'https://'
+    host = 'moppsavpn02-int.internal.das'
+    check = '/dana-na/healthcheck/healthcheck.cgi?status=all'
+
+    url = ssl + host + check
+    page_response = requests.get(url, verify=False)
+    page_content = BeautifulSoup(page_response.text, "html.parser")
+
+
+    ###########################################################################################
+    # moppsavpn03.mgmt.wellpoint.com
+    ###########################################################################################
+    ssl = 'https://'
+    host = 'moppsavpn03-int.internal.das'
+    check = '/dana-na/healthcheck/healthcheck.cgi?status=all'
+
+    url = ssl + host + check
+    page_response = requests.get(url, verify=False)
+    page_content = BeautifulSoup(page_response.text, "html.parser")
+
+
 
 
     pulse_secure = {
-        "results" : (page_content)
-
+        'host_name01': "vappsavpn01",
+        "results01" : (page_content),
+        'host_name02': "vappsavpn02",
+        "results02" : (page_content),
+        'host_name03': "vappsavpn03",
+        "results03" : (page_content),
+        'host_name_mo01': "moppsavpn01",
+        "results_mo01" : (page_content),
+        'host_name_mo02': "moppsavpn02",
+        "results_mo02" : (page_content),
+        'host_name_mo03': "moppsavpn03",
+        "results_mo03" : (page_content),
     }
-
 
     return render(request, 'pulse_secure/index.html', context=pulse_secure)
 
