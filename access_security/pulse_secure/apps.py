d08fa1d18fce9beaefb46cc228df50b21c6eb88c
Jason P. McNulty <AC89458@LC02R835TFVH8>
2019-03-20 07:42:23 -0400

    adding files

--- access_security/pulse_secure/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PulseSecureConfig(AppConfig):
-    name = 'pulse_secure'
--- access_security/pulse_secure/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-app_name = 'pulse_secure'
-
-urlpatterns = [
-    path('', views.pulse_secure, name='pulse_secure'),
-]
